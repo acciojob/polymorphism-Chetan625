f1989dbd9a1a3ecc64d65f02d92ca981dcca7660
Chetan625 <111893145+Chetan625@users.noreply.github.com>
2024-04-25 22:34:04 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
+    public  static class Product {
+        public int product(int x, int y) {
+            return 1;
+        }
 
+        public int product(int x, int y, int z) {
+            return 2;
+        }
+
+        public double product(double x, double y) {
+            return 4.2;
+        }
+    }
+
+    public static void main(String[] args) {
+        //Main main = new Main(); // Creating an instance of Main class
+        Product p = new Product(); // Creating an instance of Product class
+        int x = 5, y = 10, z = 20; // Sample values for variables x, y, and z
+
+        // Printing the results
+        System.out.println(p.product(x, y)); // Calling product(int, int)
+        System.out.println(p.product(x, y, z)); // Calling product(int, int, int)
+        System.out.println(p.product(4.0, 3.0)); // Calling product(double, double)
+    }
 }
